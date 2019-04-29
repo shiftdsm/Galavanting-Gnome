2563eac32f81e949c34fbce3e636e13afa0c23e3
Nate Rauh <nate.rauh@interactiveshift.com>
2019-04-29 11:35:55 -0500

    Move react components to views dir

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './views/App';
 import * as serviceWorker from './serviceWorker';
 import { rootReducer, LOCATIONS_ACTIONS } from './store';
 import { createStore, applyMiddleware } from 'redux';
