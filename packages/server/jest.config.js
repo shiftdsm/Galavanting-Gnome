234f97b94821a46f3cf369a472723793f8aec667
Nate Rauh <nate.rauh@interactiveshift.com>
2019-04-29 12:21:55 -0500

    Add jest

---
+++ packages/server/jest.config.js
@@ -0,0 +1,27 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  errorOnDeprecated: true,
+
+  // A path to a module which exports an async function that is triggered once before all test suites
+  // globalSetup: null,
+
+  // A path to a module which exports an async function that is triggered once after all test suites
+  // globalTeardown: null,
+
+  // A set of global variables that need to be available in all test environments
+  // globals: {},
+
+  // A preset that is used as a base for Jest's configuration
+  // preset: null,
+
+  resetMocks: true,
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  // setupFiles: [],
+
+  testEnvironment: "node",
+  watchman: false
+};
