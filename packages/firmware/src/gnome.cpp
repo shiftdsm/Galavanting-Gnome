8b5275f6ab5c10125c2ad0abf9f80b76d3bbb0d3
Matt Gerst <mattgerst@gmail.com>
2019-05-07 09:31:36 -0500

    Setup LiquidCrystal library (#29)

--- packages/firmware/src/gnome.cpp
+++ packages/firmware/src/gnome.cpp
@@ -1,6 +1,20 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+#define CONTRAST_PIN 9
+#define BACKLIGHT_PIN 7
+#define CONTRAST 110
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2, BACKLIGHT_PIN, POSITIVE);
 
 void setup() {
+    pinMode(CONTRAST_PIN, OUTPUT);
+    analogWrite(CONTRAST_PIN, CONTRAST);
+
+    lcd.backlight();
+    lcd.begin(16, 2);
+    lcd.home();
+
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
