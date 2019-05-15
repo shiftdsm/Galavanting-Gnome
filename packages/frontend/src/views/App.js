061d19977d9b006b6ac97ba9c6ec0b3bbdabf089
Nate Rauh <nate.rauh@interactiveshift.com>
2019-05-14 21:11:29 -0500

    Grid for about project text

--- packages/frontend/src/views/App.js
+++ packages/frontend/src/views/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import LocationList from './LocationList';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import GnomeMap from './GnomeMap';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -9,7 +10,6 @@ const styles = theme => ({
     textAlign: 'center',
   },
   about: {
-    textAlign: 'center',
     marginTop: '2.5rem',
   },
   aboutBody: {
@@ -29,15 +29,21 @@ const App = (props) => {
       <GnomeMap />
 
       <div className={classes.about}>
-        <Typography variant="h2" gutterBottom>
-          About This Project
-        </Typography>
+        <Grid container justify={'center'}>
+          <Grid item xs={12}>
+            <Typography variant="h2" gutterBottom className={classes.title}>
+              About This Project
+            </Typography>
+          </Grid>
 
-        <Typography variant="body1" gutterBottom className={classes.aboutBody}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-          unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-          dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-        </Typography>
+          <Grid item sm={9}>
+            <Typography variant="body1" gutterBottom className={classes.aboutBody}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+              unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+              dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            </Typography>
+          </Grid>
+        </Grid>
       </div>
 
       {/* <LocationList /> */}
