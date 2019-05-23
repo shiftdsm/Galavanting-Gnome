3eccd28ad6a0888e3796b310a4e311e2d27bea67
Matt Gerst <mattgerst@gmail.com>
2019-05-22 22:43:11 -0500

    Add methods to convert and send GPS location data

--- packages/firmware/include/gps.h
+++ packages/firmware/include/gps.h
@@ -1,5 +1,10 @@
 #pragma once
+#include <SoftwareSerial.h>
 #include <Adafruit_FONA.h>
+#include <ArduinoJson.h>
+
+#define JSON_DOC_SIZE JSON_OBJECT_SIZE(2)
+#define POST_URL "https://galavanting-gnome.herokuapp.com/location"
 
 typedef struct {
     float latitude;
@@ -13,3 +18,6 @@ typedef struct {
 } gps_loc_t;
 
 gps_loc_t getGPS(Adafruit_FONA* fona);
+StaticJsonDocument<JSON_DOC_SIZE> convertLocation(gps_loc_t location);
+String writeLocation(gps_loc_t location);
+uint16_t sendLocation(Adafruit_FONA* fona);
--- packages/firmware/src/gps.cpp
+++ packages/firmware/src/gps.cpp
@@ -10,3 +10,31 @@ gps_loc_t getGPS(Adafruit_FONA* fona) {
     location.speed_mph = location.speed_kph * 0.621371192;
     return location;
 }
+
+StaticJsonDocument<JSON_DOC_SIZE> convertLocation(gps_loc_t location) {
+    StaticJsonDocument<JSON_DOC_SIZE> doc;
+
+    doc["lat"] = location.latitude;
+    doc["lon"] = location.longitude;
+
+    return doc;
+}
+
+String writeLocation(gps_loc_t location) {
+    auto location_data = convertLocation(location);
+    String converted;
+    serializeJson(location_data, converted);
+    return converted;
+}
+
+uint16_t sendLocation(Adafruit_FONA* fona) {
+    auto location = getGPS(fona);
+    auto converted = writeLocation(location);
+
+    uint16_t status, datalen;
+    fona->HTTP_POST_start(POST_URL, F("application/json"),
+        (uint8_t *)converted.c_str(), converted.length(), &status, &datalen);
+    fona->HTTP_POST_end();
+
+    return status;
+}
