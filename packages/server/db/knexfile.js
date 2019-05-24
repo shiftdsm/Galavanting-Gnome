5af229e6ffc3d0259ad433f43fe3f2d5afa5349f
Matt Gerst <matt.gerst@interactiveshift.com>
2019-05-24 00:59:08 -0500

    Copy over nate's stuff from heroku

--- packages/server/db/knexfile.js
+++ packages/server/db/knexfile.js
@@ -29,29 +29,9 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
+    connection: porcess.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
