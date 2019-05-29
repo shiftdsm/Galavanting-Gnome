981c176e017b2d99bce4b06828d7c4403001e31b
Nate Rauh <nate.rauh@interactiveshift.com>
2019-05-28 22:23:44 -0500

    Create MainText component with boilerplate

--- packages/frontend/src/views/App.js
+++ packages/frontend/src/views/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
 import GnomeMap from './GnomeMap';
 import { withStyles } from '@material-ui/core/styles';
-import CreditList from './CreditList';
+// import CreditList from './CreditList';
+import MainText from './MainText';
 
 const styles = () => ({
   title: {
@@ -28,26 +28,8 @@ const App = (props) => {
       </Typography>
 
       <GnomeMap />
-
-      <div className={classes.about}>
-        <Grid container justify={'center'}>
-          <Grid item xs={12}>
-            <Typography variant="h2" gutterBottom className={classes.title}>
-              About This Project
-            </Typography>
-          </Grid>
-
-          <Grid item sm={9}>
-            <Typography variant="body1" gutterBottom className={classes.aboutBody}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-              unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-              dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-            </Typography>
-
-            <CreditList />
-          </Grid>
-        </Grid>
-      </div>
+      <MainText />
+      {/* <CreditList /> */}
     </div>
   );
 };
---
+++ packages/frontend/src/views/MainText.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+  },
+}));
+
+const MainText = () => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="md">
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </Paper>
+    </Container>
+  );
+};
+
+export default MainText;
