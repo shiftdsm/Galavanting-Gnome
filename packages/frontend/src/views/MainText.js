e0cc175315868386b819405e18bf60c41203fb01
Nate Rauh <nate.rauh@interactiveshift.com>
2019-05-28 22:42:24 -0500

    Add basic info sections

--- packages/frontend/src/views/MainText.js
+++ packages/frontend/src/views/MainText.js
@@ -7,8 +7,14 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2),
+    marginTop: theme.spacing(3),
+  },
+  section: {
+    marginBottom: theme.spacing(4),
   },
 }));
+
+
 
 const MainText = () => {
   const classes = useStyles();
@@ -16,12 +22,82 @@ const MainText = () => {
   return (
     <Container component="main" maxWidth="md">
       <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
-          This is a sheet of paper.
-        </Typography>
-        <Typography component="p">
-          Paper can be used to build surface or other elements for your application.
-        </Typography>
+        <div className={classes.section}>
+          <Typography variant="h4" component="h2" gutterBottom>
+            What is a Galavanting  Gnome?
+          </Typography>
+          <Typography component="p" gutterBottom>
+            First off... he has a name, its Gulliver!
+            He is a trackable traveler who tags along on any adventure you’d like to take him on.
+            Hand him off to friends to travel or leave him in a location to hitchhike his way to some amazing destinations.
+          </Typography>
+        </div>
+
+        <div className={classes.section}>
+          <Typography variant="h5" component="h3" gutterBottom>
+            What does a Galavanting Gnome do?
+          </Typography>
+          <Typography component="p" gutterBottom>
+            It's really up to the adventurer that has picked him up!
+            He can ride a bike, backpack, ride a horse, go four wheeling, or even relax on a beach.
+            The real fun is following Gulliver on his expedition with everyone.
+            As he travels, he tracks his journey at https://galavanting-gnome.surge.sh/ and allows all the fun to be shared on his social media.
+          </Typography>
+          <Typography component="i">
+            NOTE: By picking up the galavanting gnome, you are explicitly agreeing to the tracking of your location as participation of his adventuring.
+            If this is not something you want, please leave Gulliver where you find him.
+          </Typography>
+        </div>
+
+        <div className={classes.section}>
+          <Typography variant="h5" component="h3" gutterBottom>
+            How does a Galavanting Gnome work?
+          </Typography>
+          <Typography component="p">
+            Gulliver lives in 2019 and is equipped with the capability to connect to 2G networks to identify his location every 30 minutes and send his location online for an application to track his whereabouts.
+            With each ping of his travel, a path connector is drawn to give a visual of where he started and where he’s gone.
+          </Typography>
+        </div>
+
+        <div className={classes.section}>
+          <Typography variant="h5" component="h3" gutterBottom>
+            What do I do if I find the Galavanting Gnome?
+          </Typography>
+          <Typography component="p">
+            Don't panic!
+            You have already made the first step by visiting this webpage, so that you have a better idea of what to expect and how to handle Gulliver.
+            In the Owner Section, you can identify what exactly is expected of you during your time together.
+          </Typography>
+        </div>
+
+        <div className={classes.section}>
+          <Typography variant="h5" component="h3" gutterBottom>
+            Is there etiquette to having a Galavanting Gnome?
+          </Typography>
+          <Typography component="p" gutterBottom>
+            Use the Golden Rule when you find a Gulliver!
+            When on vacation, you want to see sites and do fun things, not sit in a trunk or on a shelf.
+            The same applies to our short statured friend.
+            It is our goal to see the gnome travel as much as possible!
+          </Typography>
+          <Typography compoent="p">
+            If you plan on holding Gulliver for more than a few weeks or have damaged him by accident, do not hesitate to reach out to us for support [support email]
+          </Typography>
+        </div>
+
+        <div className={classes.section}>
+          <Typography variant="h5" component="h3" gutterButtom>
+            How do I get a Galavanting Gnome?
+          </Typography>
+          <Typography component="p">
+            Visit [website] and identify where the gnome may be stationary for an extended amount of time.
+            He is probably awaiting his next travel partner, so do not hesitate to pick him up and begin your planning.
+          </Typography>
+          <Typography component="i">
+            NOTE: At this time, if a user holds on to the gnome for too long, it will appear he is ready for pickup at your own residence.
+            It is highly discouraged to search for, or leave, Gulliver in residential areas.
+          </Typography>
+        </div>
       </Paper>
     </Container>
   );
