92769a27a7c45296d60c6b6c347fe4fb4920217b
Nate Rauh <NRauh@users.noreply.github.com>
2019-06-15 20:39:00 -0500

    Check distance before publishing location (#57)
    * Check distance before publishing location
    
    * Adding publishes all unpublished that it can
    
    * Use reduce to batch publishing
    
    * No need to order unpublished locations

--- packages/server/src/services/location-service.js
+++ packages/server/src/services/location-service.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const { getDistance } = require('geolib');
 const db = require('../db');
 
 const LocationService = {
+  MIN_DISTANCE_METERS: 100,
+
   async getLocations() {
     // need to make seed data have different times
     const locations = await db.select().from('locations').orderBy('id', 'desc');
@@ -11,21 +14,42 @@ const LocationService = {
 
   async addLocation(props) {
     return db.transaction(async (tx) => {
-      const lastLocation = await tx('locations')
-        .select('id')
-        .whereNull('published_at')
-        .orderBy('created_at', 'desc')
-        .first();
+      const unpublishedLocations = await tx('locations')
+        .select('id', 'lat', 'lon')
+        .whereNull('published_at');
 
-      if (lastLocation) {
-        await tx('locations').update('published_at', new Date()).where(lastLocation);
-      }
+      const locationIdsToPublish = unpublishedLocations.reduce((locations, unpublishedLocation) => {
+        if (this.canPublishLocation(props, unpublishedLocation)) {
+          return locations.concat(unpublishedLocation.id);
+        }
+
+        return locations;
+      }, []);
+
+      await tx('locations').update('published_at', new Date()).whereIn('id', locationIdsToPublish);
 
       return tx('locations').insert({
         ...props,
         published_at: null,
       }).returning('id');
     });
+  },
+
+  canPublishLocation(newLocation, previousLocation) {
+    if (!previousLocation) {
+      return false;
+    }
+
+    const distance = getDistance(
+      { latitude: newLocation.lat, longitude: newLocation.lon },
+      { latitude: previousLocation.lat, longitude: previousLocation.lon },
+    );
+
+    if (distance < this.MIN_DISTANCE_METERS) {
+      return false;
+    }
+
+    return true;
   },
 };
 
--- packages/server/src/services/location-service.test.js
+++ packages/server/src/services/location-service.test.js
@@ -44,5 +44,49 @@ describe('LocationService', () => {
       expect(prevLocation.published_at).not.toBeFalsy();
       expect(newLocation.published_at).toBeFalsy();
     });
+
+    it('should not publish the previous location if the new location is not far away enough', async () => {
+      const location = {
+        lat: 0,
+        lon: 0,
+        kph: 0,
+        heading: 0,
+        alt: 0,
+      };
+      const [prevLocationId] = await db('locations').insert(location).returning('id');
+      await LocationService.addLocation(location);
+
+      const [prevLocation] = await db('locations').select('published_at').where('id', prevLocationId);
+
+      expect(prevLocation.published_at).toBeFalsy();
+    });
+
+    it('should publish all unpublished locations until it hits one that is not far enough away', async () => {
+      const baseLocation = {
+        lat: 0,
+        lon: 0,
+        kph: 0,
+        heading: 0,
+        alt: 0,
+      };
+
+      const [firstLocationId, secondLocationId, thirdLocationId] = await db('locations')
+        .insert([
+          { ...baseLocation },
+          { ...baseLocation },
+          { ...baseLocation, lon: 0.0005 }, // 56 meters from 0
+        ])
+        .returning('id');
+
+      await LocationService.addLocation({ ...baseLocation, lon: 0.001 }); // 111 meters from 0
+
+      const [firstLocation] = await db('locations').select('published_at').where('id', firstLocationId);
+      const [secondLocation] = await db('locations').select('published_at').where('id', secondLocationId);
+      const [thirdLocation] = await db('locations').select('published_at').where('id', thirdLocationId);
+
+      expect(firstLocation.published_at).toBeTruthy();
+      expect(secondLocation.published_at).toBeTruthy();
+      expect(thirdLocation.published_at).toBeFalsy();
+    });
   });
 });
